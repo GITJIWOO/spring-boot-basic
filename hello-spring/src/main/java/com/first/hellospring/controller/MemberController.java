97acd222750b2aa93d5a42d7fd2064d7b503c6dc
PYOJITAE <kjw011231@gmail.com>
2021-12-03 02:00:11 +0900

    DB Access

--- hello-spring/src/main/java/com/first/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/com/first/hellospring/controller/MemberController.java
@@ -1,10 +1,19 @@
 package com.first.hellospring.controller;
 
+import com.first.hellospring.domain.Member;
+import com.first.hellospring.domain.MemberForm;
 import com.first.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
@@ -12,6 +21,28 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/new")
+    public String createForm() {
+        return "/members/createMemberForm";
+    }
+
+    @PostMapping("/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/memberList")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "/members/memberList";
     }
 
 }
---
+++ hello-spring/src/main/java/com/first/hellospring/domain/MemberForm.java
@@ -0,0 +1,10 @@
+package com.first.hellospring.domain;
+
+import lombok.Data;
+
+@Data
+public class MemberForm {
+
+    private String name;
+
+}
