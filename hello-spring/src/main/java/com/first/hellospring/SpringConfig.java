a216a4a2325b78359a347e4093e85fccf0e70520
PYOJITAE <kjw011231@gmail.com>
2021-12-04 07:00:01 +0900

    H2, JPA

--- hello-spring/src/main/java/com/first/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/first/hellospring/SpringConfig.java
@@ -1,22 +1,37 @@
 package com.first.hellospring;
 
-import com.first.hellospring.repository.MemberRepository;
-import com.first.hellospring.repository.MemoryMemberRepository;
+import com.first.hellospring.repository.*;
 import com.first.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     // 자바 코드로 Spring Bean 의존성 주입 하는 법
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
+    /*
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        // return new JdbcTemplateMemberRepository(dataSource);
+        // return new JpaMemberRepository(em);
     }
+    */
+
 }
